08ce29c6c8714dd97568c6e7c962ba75c5f93962
Doug MacEwen <d2macewen@gmail.com>
2019-07-11 17:16:26 -0700

    Change beta user table to reflect awknowledgement

--- create_beta_testers_table.sql
+++ create_beta_testers_table.sql
@@ -1,7 +1,6 @@
 CREATE TABLE beta_testers (
     user_id INTEGER,
-    signed_nda BOOLEAN NOT NULL,
-    nda_path TEXT,
+    acknowledge_confidentiality BOOLEAN NOT NULL,
     PRIMARY KEY (user_id),
     FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
