b8ce1b27b982ae2acd3110c85b991ce5625c0291
Doug MacEwen <d2macewen@gmail.com>
2019-07-14 13:33:59 -0700

    insert creation time when row is made

--- create_capture_sessions_table.sql
+++ create_capture_sessions_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE capture_sessions (
     session_id SERIAL,
     user_id INTEGER NOT NULL,
     skin_color_id INTEGER NOT NULL,
-    start_date TIMESTAMP NOT NULL,
+    start_date TIMESTAMP NOT NULL DEFAULT CURRENT_DATE,
     out_of_date BOOL NOT NULL,
     PRIMARY KEY (session_id),
     FOREIGN KEY (user_id) REFERENCES users (user_id)
