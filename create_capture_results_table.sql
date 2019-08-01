b86e7ec137f272100554867cb2d1e21de270f3a0
Doug MacEwen <d2macewen@gmail.com>
2019-07-31 17:07:04 -0700

    capture results now defaults to using NOW()::TIMESTAMP

--- create_capture_results_table.sql
+++ create_capture_results_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE capture_results (
     capture_id INTEGER,
     user_id INTEGER NOT NULL,
     backend_version TEXT NOT NULL,
-    processed_date TIMESTAMP NOT NULL,
+    processed_date TIMESTAMP NOT NULL DEFAULT NOW()::TIMESTAMP,
     calibrated_skin_color FLOAT [],
     matched_skin_color_id INTEGER,
     PRIMARY KEY (capture_id),
