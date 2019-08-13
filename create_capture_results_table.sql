554c01fbc595e72e1748cf0a81096f86b2a44080
Doug MacEwen <d2macewen@gmail.com>
2019-08-13 15:32:09 -0700

    add successful column

--- create_capture_results_table.sql
+++ create_capture_results_table.sql
@@ -6,6 +6,7 @@ CREATE TABLE capture_results (
     processed_date TIMESTAMP NOT NULL DEFAULT NOW()::TIMESTAMP,
     calibrated_skin_color FLOAT [],
     matched_skin_color_id INTEGER,
+    successful BOOLEAN NOT NULL DEFAULT FALSE,
     PRIMARY KEY (capture_id),
     FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
