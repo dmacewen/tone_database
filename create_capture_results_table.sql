f84894241d5f49c305dd0af20d6f3eeddb588d91
Doug MacEwen <d2macewen@gmail.com>
2019-09-06 12:01:06 -0700

    added dev color field to capture results

--- create_capture_results_table.sql
+++ create_capture_results_table.sql
@@ -5,6 +5,7 @@ CREATE TABLE capture_results (
     backend_version TEXT NOT NULL,
     processed_date TIMESTAMP NOT NULL DEFAULT NOW()::TIMESTAMP,
     calibrated_skin_color FLOAT [],
+    dev_calibrated_skin_color FLOAT [],
     matched_skin_color_id INTEGER,
     successful BOOLEAN NOT NULL DEFAULT FALSE,
     PRIMARY KEY (capture_id),
